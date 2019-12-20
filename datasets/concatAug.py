3e59abac9737052a7b158cb5b3c342284dc3bae0
sudachi0114 <jnevariste@gmail.com>
2019-12-20 15:03:44 +0000

    main 関数を追加 / 見やすいように整形 / check 処理を追加

--- datasets/concatAug.py
+++ datasets/concatAug.py
@@ -23,7 +23,7 @@ save_1_loc = os.path.join(save_loc, class_list[1])
 os.makedirs(save_1_loc, exist_ok=True)
 
 
-def mv(src_dir, file_list, dist_dir, param=None):
+def copy(src_dir, file_list, dist_dir, param=None):
 
     for pic_name in file_list:
         copy_src = os.path.join(src_dir, pic_name)
@@ -37,50 +37,70 @@ def mv(src_dir, file_list, dist_dir, param=None):
         shutil.copy(copy_src, copy_dst)
 
 
+def main():
 
-train_0_dir = os.path.join(train_dir, class_list[0])
-train_0_list = os.listdir(train_0_dir)
-print(train_0_dir)
-print("get {} data".format(len(train_0_list)))
+    # class 0 ==========
+    train_0_dir = os.path.join(train_dir, class_list[0])
+    train_0_list = os.listdir(train_0_dir)
+    print(train_0_dir)
+    print("get {} data".format(len(train_0_list)))
 
-print("move.....")
-mv(train_0_dir, train_0_list, save_0_loc)
-print("    Done.")
-
-
-train_1_dir = os.path.join(train_dir, class_list[1])
-train_1_list = os.listdir(train_1_dir)
-print(train_1_dir)
-print("get {} data".format(len(train_1_list)))
-
-print("move.....")
-mv(train_1_dir, train_1_list, save_1_loc)
-print("    Done.")
-
-
-# ----------
-
-for i in range(2):
-    print("その {} ----------".format(i))
-    auged_dir = os.path.join(cwd, "auged_{}".format(i))
-
-    auged_0_dir = os.path.join(auged_dir, class_list[0])
-    auged_0_list = os.listdir(auged_0_dir)
-    print(auged_0_dir)
-    print("get {} data".format(len(auged_0_list)))
-
-    print("move.....")
-    mv(auged_0_dir, auged_0_list, save_0_loc, param=i)
-    print("    Done.")
-
-    auged_1_dir = os.path.join(auged_dir, class_list[1])
-    auged_1_list = os.listdir(auged_1_dir)
-    print(auged_1_dir)
-    print("get {} data".format(len(auged_1_list)))
-
-    print("move.....")
-    mv(auged_1_dir, auged_1_list, save_1_loc, param=i)
+    print("copy.....")
+    copy(train_0_dir, train_0_list, save_0_loc)
     print("    Done.")
 
 
+    # class 1 ==========
+    train_1_dir = os.path.join(train_dir, class_list[1])
+    train_1_list = os.listdir(train_1_dir)
+    print(train_1_dir)
+    print("get {} data".format(len(train_1_list)))
 
+    print("copy.....")
+    copy(train_1_dir, train_1_list, save_1_loc)
+    print("    Done.")
+
+
+    # augmented data -----
+    for i in range(2):
+        print("その {} ----------".format(i))
+        auged_dir = os.path.join(cwd, "auged_{}".format(i))
+
+        # class 0 ==========
+        auged_0_dir = os.path.join(auged_dir, class_list[0])
+        auged_0_list = os.listdir(auged_0_dir)
+        print(auged_0_dir)
+        print("get {} data".format(len(auged_0_list)))
+
+        print("copy.....")
+        copy(auged_0_dir, auged_0_list, save_0_loc, param=i)
+        print("    Done.")
+
+        # class 1 ==========
+        auged_1_dir = os.path.join(auged_dir, class_list[1])
+        auged_1_list = os.listdir(auged_1_dir)
+        print(auged_1_dir)
+        print("get {} data".format(len(auged_1_list)))
+
+        print("copy.....")
+        copy(auged_1_dir, auged_1_list, save_1_loc, param=i)
+        print("    Done.")
+
+
+
+def check():
+
+    auged_train_dir = os.path.join(cwd, "train_with_aug")
+
+    auged_train_0_dir = os.path.join(auged_train_dir, class_list[0])
+    print(auged_train_0_dir)
+    print( len(os.listdir(auged_train_0_dir)) )
+
+    auged_train_1_dir = os.path.join(auged_train_dir, class_list[1])
+    print(auged_train_1_dir)
+    print( len(os.listdir(auged_train_1_dir)) )
+
+
+if __name__ == "__main__":
+    main()
+    check()
