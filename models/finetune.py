0c051e5c0f142891a1a94704fdb019bbe462f0da
sudachi <jnevariste@gmail.com>
2020-01-10 10:59:06 +0000

    data の分岐を追加

--- models/finetune.py
+++ models/finetune.py
@@ -32,12 +32,13 @@ def main():
     data_dir = os.path.join(prj_root, "datasets")
 
     # original train_data only or with_augmented data
-    train_dir = os.path.join(data_dir, "train")
-    # train_dir = os.path.join(data_dir, "train_with_aug")
-    validation_dir = os.path.join(data_dir, "val")  # original validation data
+    # train_dir = os.path.join(data_dir, "train")
+    train_dir = os.path.join(data_dir, "train_with_aug")
+    # validation_dir = os.path.join(data_dir, "val")  # original validation data
 
     # pair of decreaced train_data and increased validation data
     # train_dir = os.path.join(data_dir, "red_train")
+    # train_dir = os.path.join(data_dir, "train_with_aug")
     # validation_dir = os.path.join(data_dir, "validation")
 
     test_dir = os.path.join(data_dir, "test")
