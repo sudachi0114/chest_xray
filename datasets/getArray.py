60695b24f879aabefe48ec1148a9c01c8dd932af
suzuki daichi <jnevariste@gmail.com>
2020-01-11 00:18:17 +0900

    関数化と loop 処理にまとめる

--- datasets/getArray.py
+++ datasets/getArray.py
@@ -11,8 +11,8 @@ cwd = os.getcwd()
 
 train_dir = os.path.join(cwd, "train")
 
+# sheve class_list ----------
 class_list = os.listdir(train_dir)
-# sheve ----------
 for fname in ignore_files:
     if fname in class_list:
         class_list.remove(fname)
@@ -22,64 +22,44 @@ class_list = sorted(class_list)
 train_0_dir = os.path.join(train_dir, class_list[0])
 train_1_dir = os.path.join(train_dir, class_list[1])
 
-validation_dir = os.path.join(cwd, "val")
-validation_0_dir = os.path.join(validation_dir, class_list[0])
-validation_1_dir = os.path.join(validation_dir, class_list[1])
-
-test_dir = os.path.join(cwd, "test")
-test_0_dir = os.path.join(test_dir, class_list[0])
-test_1_dir = os.path.join(test_dir, class_list[1])
-
-train_0_dir_list = sorted(os.listdir(train_0_dir))
-
-# class 0 ----------
-#print(train_0_dir_list[0])
-
-start = time.time()
-
-img_list = []
-img_size = 224
-channel = 1
-img_shape = (img_size, img_size)
-for i in range(len(train_0_dir_list)):
-    targf = os.path.join(train_0_dir, train_0_dir_list[i])
-
-    pil_obj = Image.open(targf)
-    pil_obj = pil_obj.resize(img_shape)
-    #pil_obj.convert("L")
-    #print(pil_obj)
-
-    img_arr = np.array(pil_obj)
-    #print(img_arr.shape)
-    assert img_arr.shape == img_shape
-    img_list.append(img_arr)
-
-img_list = np.array(img_list)
-print("\nimg_list shape: ", img_list.shape)
-
-print("\nelaped time: {} [s]".format(time.time() - start))
 
 
-# class 1 ----------
-train_1_dir_list = sorted(os.listdir(train_1_dir))
-img_1_list = []
-for i in range(len(train_1_dir_list)):
-    targf = os.path.join(train_1_dir, train_1_dir_list[i])
+# sample
+print( os.path.join(train_0_dir, os.listdir(train_0_dir)[0]) )
 
-    pil_obj = Image.open(targf)
-    pil_obj = pil_obj.resize(img_shape)
-    pil_obj = pil_obj.convert("L")
-    #print(pil_obj)
+def load_datas_from_dir(target_dir):
 
-    img_arr = np.array(pil_obj)
-    #print(img_arr.shape)
-    assert img_arr.shape == img_shape, "Error {} => shape: {} | fname: {}".format(i, img_arr.shape, targf)
-    img_1_list.append(img_arr)
+    start = time.time()
 
-img_1_list = np.array(img_1_list)
-print("\nimg_list shape: ", img_1_list.shape)
+    img_list = []
+    img_size = 224
+    channel = 1
+    img_shape = (img_size, img_size)
 
-print("\nelaped time1: {} [s]".format(time.time() - start))
+    print("\nloading data from: ", target_dir)
+    target_list = sorted(os.listdir(target_dir))
+
+    for i in range(len(target_list)):
+        targf = os.path.join(target_dir, target_list[i])
+
+        pil_obj = Image.open(targf)
+        pil_obj = pil_obj.resize(img_shape)
+        pil_obj = pil_obj.convert("L")
+        # print(pil_obj)
+
+        img_arr = np.array(pil_obj)
+        # print(img_arr.shape)
+        assert img_arr.shape == img_shape
+        img_list.append(img_arr)
+
+    img_list = np.array(img_list)
+    print("  img_list shape: ", img_list.shape)
+    print("  elaped time: {} [sec]".format(time.time() - start))
+
+
+load_datas_from_dir(train_0_dir)
+load_datas_from_dir(train_1_dir)
+
 """
 for i in range(10):
     plt.subplot(2, 5, i+1)
@@ -88,3 +68,13 @@ for i in range(10):
     plt.title(i)
 plt.show()
 """
+
+rest_data_source = ["val", "test"]
+
+for dname in rest_data_source:
+    source_dir = os.path.join(cwd, dname)
+    for i in range( len(class_list) ):
+        dtarget = os.path.join(source_dir, class_list[i])
+        load_datas_from_dir(dtarget)
+
+
